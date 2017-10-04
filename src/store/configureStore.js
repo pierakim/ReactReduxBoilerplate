d71d064815fd60b9f643dea85fd86f7e5cf79a83
pierakim <pierakim.lambda@gmail.com>
2017-10-04 22:26:07 +1000

    Add some conf for react dev tool

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
@@ -8,6 +8,12 @@ export default function configureStore(initialState){
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(reduxImmutableStateInvariant())
+        compose(
+            applyMiddleware(reduxImmutableStateInvariant()),
+            // enable/disable ReactDevTool extension here
+            // todo:
+            // separate dev and prod
+            //window.devToolsExtension ? window.devToolsExtension() : f => f
+        )
     );
 }
